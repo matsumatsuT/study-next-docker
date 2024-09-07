999f3d600e433ede77a42202f735fba597a22ed6
matsumoto <dsongben@gmail.com>
2024-09-07 15:20:30 +0900

    test用にページ追加

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Page() {
+  return (
+    <>
+      <h1>Hello, Home page TEST!!!</h1>
+      <Link href="/other">画面遷移</Link>
+    </>
+  )
+}
